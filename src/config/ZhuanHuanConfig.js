4a58a1f0a84a658405a15cb02136af2910cc5c35
whiteclouds <whiteclouds1971@163.com>
2024-11-10 00:04:29 +0800

    feature:转换计

--- src/config/ZhuanHuanConfig.js
+++ src/config/ZhuanHuanConfig.js
@@ -64,4 +64,117 @@ export default [
       },
     ],
   },
+  {
+    id: 'adf49667-d451-45c8-a8fb-ac9d439fae05',
+    code: '',
+    name: '举棋',
+    bgColor: color.color1,
+    text: '转换技，阳：准备阶段，你摸三张牌/其他角色的准备阶段，其可以展示并交给你一张黑色手牌；阴：准备阶段，你本回合使用牌无次数限制且伤害+1/其他角色的准备阶段，其可以展示并交给你一张红色手牌。',
+    skills: [
+      {
+        cover: './YinYangZhuanHuan/阳.jpeg',
+        code: '阳',
+        name: '',
+        keywords: [
+          '阳：准备阶段，你摸三张牌/其他角色的准备阶段，其可以展示并交给你一张黑色手牌；',
+        ],
+      },
+      {
+        cover: './YinYangZhuanHuan/阴.jpeg',
+        code: '阴',
+        name: '',
+        keywords: [
+          '阴：准备阶段，你本回合使用牌无次数限制且伤害+1/其他角色的准备阶段，其可以展示并交给你一张红色手牌。',
+        ],
+      },
+    ],
+  },
+  {
+    id: '5b1d1d73-84d8-4dbf-b1f2-256f74c58ce9',
+    code: '',
+    name: '游龙',
+    bgColor: color.color6,
+    text: '转换技，阳：每轮限一次，你可以废除你装备区里的一个装备栏，视为使用一张未以此法使用过的普通锦囊牌；阴：每轮限一次，你可以废除你装备区里的一个装备栏，视为使用一张未以此法使用过的基本牌。',
+    skills: [
+      {
+        cover: './YinYangZhuanHuan/阳.jpeg',
+        code: '阳',
+        name: '',
+        keywords: [
+          '阳：每轮限一次，你可以废除你装备区里的一个装备栏，视为使用一张未以此法使用过的普通锦囊牌；',
+        ],
+      },
+      {
+        cover: './YinYangZhuanHuan/阴.jpeg',
+        code: '阴',
+        name: '',
+        keywords: [
+          '阴：每轮限一次，你可以废除你装备区里的一个装备栏，视为使用一张未以此法使用过的基本牌。',
+        ],
+      },
+    ],
+  },
+  {
+    id: 'dc373cfb-6a66-4e28-8405-cedde8f36297',
+    code: '',
+    name: '自若',
+    bgColor: color.color4,
+    text: '转换技，锁定技，阳：当你使用最左侧的手牌时，你摸一张牌。阴：当你使用最右侧的手牌时，你摸一张牌。若如此做，你本回合不能整理手牌。',
+    skills: [
+      {
+        cover: './YinYangZhuanHuan/阳.jpeg',
+        code: '阳',
+        name: '',
+        keywords: ['阳：当你使用最左侧的手牌时，你摸一张牌。'],
+      },
+      {
+        cover: './YinYangZhuanHuan/阴.jpeg',
+        code: '阴',
+        name: '',
+        keywords: ['阴：当你使用最右侧的手牌时，你摸一张牌。'],
+      },
+    ],
+  },
+  {
+    id: '481f3688-c691-482d-b2d9-600c35e8a352',
+    code: '',
+    name: '东道',
+    bgColor: color.color5,
+    text: '转换技，农民回合结束后，阳：你可以令地主执行一个额外回合。阴：其可以执行一个额外回合。',
+    skills: [
+      {
+        cover: './YinYangZhuanHuan/阳.jpeg',
+        code: '阳',
+        name: '',
+        keywords: ['阳：你可以令地主执行一个额外回合。'],
+      },
+      {
+        cover: './YinYangZhuanHuan/阴.jpeg',
+        code: '阴',
+        name: '',
+        keywords: ['阴：其可以执行一个额外回合。'],
+      },
+    ],
+  },
+  {
+    id: 'd89f8ea5-d828-4b5f-b0d5-332df57be696',
+    code: '',
+    name: '观骨',
+    bgColor: color.color1,
+    text: '转换技，出牌阶段限一次，阳：你可以观看牌堆顶至多四张牌；阴：你可以观看一名角色至多四张手牌。然后你可以使用其中一张牌。',
+    skills: [
+      {
+        cover: './YinYangZhuanHuan/阳.jpeg',
+        code: '阳',
+        name: '',
+        keywords: ['阳：你可以观看牌堆顶至多四张牌；'],
+      },
+      {
+        cover: './YinYangZhuanHuan/阴.jpeg',
+        code: '阴',
+        name: '',
+        keywords: ['阴：你可以观看一名角色至多四张手牌。'],
+      },
+    ],
+  },
 ];
