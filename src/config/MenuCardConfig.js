4810d359c622f8b3d4f3266ca356f265a0881687
whiteclouds <whiteclouds1971@163.com>
2024-11-10 14:41:46 +0800

    feature:游戏牌

--- src/config/MenuCardConfig.js
+++ src/config/MenuCardConfig.js
@@ -6,12 +6,47 @@ export default [
     name: '体力卡',
     url: '/tlk',
   },
+  // 游戏牌
+  {
+    code: '游戏牌',
+    cover: './MenuImage/游戏牌.jpg',
+    name: '游戏牌',
+    url: '/yxp',
+  },
   // 标记
   {
     code: 'BJ',
     cover: './MenuImage/标记.jpg',
     name: '标记',
     url: '/bj',
+  },
+  // 阴阳转化
+  {
+    code: 'YINYANGZHUANHUAN',
+    cover: './MenuImage/阴阳转化.png',
+    name: '阴阳转化',
+    url: '/yyzh',
+  },
+  // 选中势力
+  {
+    code: 'XUANZHESHILI',
+    cover: './MenuImage/势力卡.png',
+    name: '势力卡',
+    url: '/xlk',
+  },
+  // 谋奕
+  {
+    code: 'MOUYI',
+    cover: './MenuImage/谋奕.jpg',
+    name: '谋奕',
+    getModal: data => {
+      return {
+        component: () => import('@/modal/My.vue'),
+        modal: {
+          title: '谋奕',
+        },
+      };
+    },
   },
   // 记录角色位置
   {
@@ -47,40 +82,5 @@ export default [
         },
       };
     },
-  },
-  // 谋奕
-  {
-    code: 'MOUYI',
-    cover: './MenuImage/谋奕.jpg',
-    name: '谋奕',
-    getModal: data => {
-      return {
-        component: () => import('@/modal/My.vue'),
-        modal: {
-          title: '谋奕',
-        },
-      };
-    },
-  },
-  // 阴阳转化
-  {
-    code: 'YINYANGZHUANHUAN',
-    cover: './MenuImage/阴阳转化.png',
-    name: '阴阳转化',
-    url: '/yyzh',
-  },
-  // 选中势力
-  {
-    code: 'XUANZHESHILI',
-    cover: './MenuImage/势力卡.png',
-    name: '势力卡',
-    url: '/xlk',
-  },
-  // 游戏牌
-  {
-    code: '游戏牌',
-    cover: './MenuImage/游戏牌.jpg',
-    name: '游戏牌',
-    url: '/yxp',
   },
 ];
